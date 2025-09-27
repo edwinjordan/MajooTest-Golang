fb3c1112ccb63beda62002086cec557d741a9ebc
edwinjordan <edwinlaksono12@gmail.com>
2025-09-27 09:03:40 +0700

    feat : add schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+-- Enable UUID generator
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(), 
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+    deleted_at TIMESTAMP WITH TIME ZONE DEFAULT NULL
+
+);
